8e08a964a89371aa8377eebfce02b60c80086cbd
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-01-29 19:42:05 -0500

    add clone append

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,12 @@ $(function () {
   let date = moment();
   var dateToday = $("#currentDay");
   dateToday.text(date.format('ddd MMM Do YYYY'));
+
+  let timebloks = $('#timeblocks');
+  let masterRow = $('#day-row');
+  masterRow.children('.hourname').text('12:00 AM');
+
+  let newRow = masterRow.clone();
+  timebloks.append(newRow);
+
 });
