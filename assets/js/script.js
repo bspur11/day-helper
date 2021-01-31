d79024dcbe808348c8f26f0dfb1cbea380e12f28
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-01-31 13:24:41 -0500

    nwe HTML and new js start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,42 @@
-$(function () {
-  let date = moment();
-  var dateToday = $("#currentDay");
-  dateToday.text(date.format('dddd MMMM Do YYYY'));
+$(document).ready(function () {
+      // saveBtn click listener 
+      $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-  let timebloks = $('#timeblocks');
-  let masterRow = $('#day-row');
-  masterRow.children('.hourname').text('12:00 AM');
+        // Save text in local storage
+        localStorage.setItem(time, text);
+      })
 
-  // js to clone a div for a loop
 
-  for (i = 0; i < 5; i++) {
-    let newRow = masterRow.clone();
-    timebloks.append(newRow);
-  }
 
-});
+
+
+
+
+      // );
+
+      // $(function () {
+      //   let date = moment();
+      //   var dateToday = $("#currentDay");
+      //   dateToday.text(date.format('dddd MMMM Do YYYY'));
+
+      //   let timebloks = $('#timeblocks');
+      //   let masterRow = $('#day-row');
+      //   let hourStamp = $('#day-row');
+      //   let taskEl = $('#timeblocks');
+      //   hourStamp.children('.hourname').text('8:00 AM');
+      //   // masterRow.children('.hourname').text('12:00 AM');
+
+      //    // js to clone a div for a loop
+
+      //     // for (i = 0; i < 9; i++) {
+      //     // let newRow = masterRow.clone();
+      //     // timebloks.append(newRow);
+      //     for (i = 0; i <= 9; i++) {
+      //       let newStamp = hourStamp.clone();
+      //       taskEl.append(newStamp);
+      //     }
+
+      //   }
