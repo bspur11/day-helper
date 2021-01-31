26f4f7b3c862854e7dc6c6d6d613aa75932852f8
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-01-30 20:06:52 -0500

    add dynamic day-row

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 $(function () {
   let date = moment();
   var dateToday = $("#currentDay");
-  dateToday.text(date.format('ddd MMM Do YYYY'));
+  dateToday.text(date.format('dddd MMMM Do YYYY'));
 
   let timebloks = $('#timeblocks');
   let masterRow = $('#day-row');
   masterRow.children('.hourname').text('12:00 AM');
 
-  let newRow = masterRow.clone();
-  timebloks.append(newRow);
+  // js to clone a div for a loop
+
+  for (i = 0; i < 5; i++) {
+    let newRow = masterRow.clone();
+    timebloks.append(newRow);
+  }
 
 });
